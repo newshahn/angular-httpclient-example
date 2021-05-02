dece7edbb30ea7e5cf07be8ddbb53e17fb490428
Newsha Hosseiny Nik <nioosha.h@gmail.com>
2021-05-02 16:26:46 +0430

    added about and home component and data service

---
+++ src/app/data.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private REST_API_SERVER: string = 'http://localhost:3000';
+
+  constructor(private httpClient: HttpClient) {}
+
+  public sendGetRequest() {
+    return this.httpClient.get(this.REST_API_SERVER);
+  }
+}
